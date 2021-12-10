7846a506837b21a51847925da698434b2fb072de
msarefin <mohammed.arefin@gmail.com>
2021-12-10 11:33:05 -0500

    added GET, POST, PUT and DELETE

--- 00_Express_Server/api/apiRouter.js
+++ 00_Express_Server/api/apiRouter.js
@@ -24,10 +24,63 @@ let employees = [
     }
 ]
 
+// Get ID 
+let getID = () =>{
+    return '_'+Math.random().toString(36).substr(2,9);
+};
+
 // GET - Employees
 
 router.get('/employees',(request, response) =>{
-    response.json(employees)
+    console.log(`GET request receives at server ...${new Date().toLocaleTimeString()}`);
+    response.json(employees);
 }); 
 
+// POST Request
+
+router.post('/employees',(request, response) =>{
+    let employee = {
+        id : getID, 
+        first_name : request.body.first_name, 
+        last_name : request.body.last_name,
+        email : request.body.email, 
+        gender : request.body.gender,
+        ip_address : request.body.ip_address
+
+    };
+    employees.push(employees);
+    console.log(`POST request receives at server ...${new Date().toLocaleTimeString()}`);
+    response.json({msg : 'POST request is successfull ...!'});
+});
+
+// PUT Request
+router.put('/employees/:id',(request,response) => {
+    let empID = request.params.id
+    let updateEmployee = {
+        id : empID,
+        first_name : request.body.first_name, 
+        last_name : request.body.last_name, 
+        email : request.body.email, 
+        gender : request.body.gender, 
+        ip_address :request.body.ip_address
+    }
+    let existingEmployee = employees.find((employee) =>{
+        return employee.id === empID; 
+    });
+    employees.splice(employees.indexOf(existingEmployee),1,updateEmployee);
+    console.log(`PUT request received at server ...${new Date().toLocaleTimeString()}`);
+    response.json({msg : 'PUT request is Successfull ...!'});
+});
+
+// DELETE  Request 
+router.delete('/employees/:id',(request, response) =>{
+    let empID = request.params.id; 
+    employees = employees.filter((employee) =>{
+        return employee.id !== empID; 
+    });
+    console.log(`DELETE Request at server ...${new Date().toLocaleTimeString()}`);
+    response.json({msg : 'DELETE request is successful ...!'});
+});
+
 module.exports = router; 
+
