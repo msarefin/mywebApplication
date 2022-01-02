1500dded497af19fefcbaf573d5b511199af98a6
msarefin <mohammed.arefin@gmail.com>
2022-01-01 20:25:27 -0500

    working on 01 app.js

--- 01_AJAX_Basics/app.js
+++ 01_AJAX_Basics/app.js
@@ -1,1 +1,12 @@
 console.log('AJAX Basics!');
+
+let textButton = document.querySelector('#text-btn');
+textButton.addEventListener('click', function(){
+    // AJAX
+
+    // Create an AJAX request
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET','./data/message.txt',true);
+});
