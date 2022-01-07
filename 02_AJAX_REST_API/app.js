078cb5711368341ba406810b61720f3184c8b475
msarefin <mohammed.arefin@gmail.com>
2022-01-07 13:43:20 -0500

    added BrainHttp.js to 02

---
+++ 02_AJAX_REST_API/app.js
@@ -0,0 +1,18 @@
+// GET Button
+let getButton = document.querySelector("#get-btn"); 
+getButton.addEventListener('click', function(){
+    fetchEmployees();
+});
+
+let fetchEmployees = () =>{
+    
+};
+
+// POST Button
+let postButtin = document.querySelector("post-btn");
+
+// PUT Button
+let putButton = document.querySelector("put-btn"); 
+
+// DELETE Button
+let deleteButton = document.querySelector("delete-btn"); 
