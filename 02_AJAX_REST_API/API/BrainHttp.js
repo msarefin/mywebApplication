fd06dc9dc477c7ef65860ed8c2b112abe97c0ec5
msarefin <mohammed.arefin@gmail.com>
2022-01-07 22:44:51 -0500

    updates

---
+++ 02_AJAX_REST_API/API/BrainHttp.js
@@ -0,0 +1,18 @@
+export class BrainHttp{
+    constructor (){
+        this.http = new XMLHttpRequest();
+    }
+
+    // GET Request
+
+    get = (url) =>{
+        this.http.open(); 
+    }
+
+
+    // PUSH Reuest
+
+    // POST Request
+
+    // DELETE Request 
+}
