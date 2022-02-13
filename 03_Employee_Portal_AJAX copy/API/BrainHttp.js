1676ad2bfce04aadfb578fbbb57db2b347d02f9a
arefin <mohammed.arefin@gmail.com>
2022-02-13 06:57:49 -0500

    added 06

---
+++ 03_Employee_Portal_AJAX copy/API/BrainHttp.js
@@ -0,0 +1,62 @@
+export class BrainHttp{
+    constructor (){
+        this.http = new XMLHttpRequest();
+    }
+
+    // GET Request
+
+    get = (url, callback) =>{
+        this.http.open('GET',url, true); 
+        this.http.send(); 
+        this.http.onload = () =>{
+            if(this.http.status ===200){
+                let data = this.http.responseText; 
+                let employees = JSON.parse(data);
+                
+                callback(null,employees);
+            }else{
+                callback(`Error: ${this.http.status}`);
+            }
+        };
+    }
+
+
+    // POST Request
+
+    post = (url, employee, callback) =>{
+        this.http.open('POST',url, true);
+        this.http.setRequestHeader('Content-type', 'application/json'); 
+        this.http.send(JSON.stringify(employee)); 
+        this.http.onload =() => {
+            let data = this.http.responseText; 
+            let employees = JSON.parse(data);
+            callback(employees);
+        } 
+    };
+
+    // PUT Request
+    put = (url, employee, callback) =>{
+        this.http.open('PUT', url, true);
+        this.http.setRequestHeader('Content-type','application/json');
+        this.http.send(JSON.stringify(employee)); 
+        this.http.onload =()=>{
+            let data = this.http.responseText;
+            let employees = JSON.parse(data);
+            callback(employees); 
+        };
+    };
+
+
+    // DELETE Request 
+    delete  =(url, callback)=>{
+        this.http.open('DELETE',url, true);
+        this.http.setRequestHeader('Content-type','application/json');
+        this.http.send();
+        this.http.onload = ()=>{
+            let data = this.http.responseText; 
+            let employees = JSON.parse(data);
+            callback(employees);
+        };
+    };
+
+}
