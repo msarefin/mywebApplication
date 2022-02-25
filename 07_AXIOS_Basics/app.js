b1f4571e1317dd591d4f71ca37703770da3e7d7b
msarefin <mohammed.arefin@gmail.com>
2022-02-25 11:42:29 -0500

    update on 07 Axios

--- 07_AXIOS_Basics/app.js
+++ 07_AXIOS_Basics/app.js
@@ -1,3 +1,4 @@
+//Text Data
 let textbutton = document.querySelector('#text-btn');
 textbutton.addEventListener('click',function(){
     fetchTextData();
@@ -10,9 +11,41 @@ let fetchTextData = ()=>{
             retuen; 
         }
         let fileContent = response.data; 
-        console.log(fileContent);
+        // console.log(fileContent);
         document.querySelector('#text-card').innerHTML = fileContent; 
     }).catch((err)=>{
         console.error(err);
     });
 };
+
+//JSON Data 
+
+let jsonButton = document.querySelector('#json-btn'); 
+jsonButton.addEventListener('click', function(){
+    fetchJSONData(); 
+});
+
+let fetchJSONData = () =>{
+    axios.get('./data/mobile.json').then((response)=>{
+        if(response.status !==200){
+            console.log(`Something went wrong: ${response.status}`);
+            return;
+        }
+        let mobile = response.data; 
+        let htmlTemplate = `
+            <ul class="list-group">
+                <li class="list-group-item">Name: ${mobile.Name}</li>
+                <li class="list-group-item">Model: ${mobile.model}</li>
+                <li class="list-group-item">Year: ${mobile.country}</li>
+                <li class="list-group-item">Country: ${mobile.country}</li>
+            </ul>`;
+        document.querySelector('#json-card').innerHTML = htmlTemplate; 
+
+    }).catch((err)=>{
+        console.error(err);
+    }); 
+};
+
+//API Data 
+let apiButton = document.querySelector('#api-btn'); 
+
